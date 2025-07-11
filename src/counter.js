228565a9d161954a4ddebfde99770b609aafd947
titiksha07 <titiksha0410@gmail.com>
2025-07-11 21:12:43 +0530

    LL

--- src/counter.js
+++ src/counter.js
@@ -306,5 +306,56 @@ var MyLinkedList = function() {
 //   head = prev;
 //   return head;
 // };
+
+/*Linked list cycle*/
+
+// var hasCycle = function(head) {
+//   let curr = head;
+//   let set = new Set();
+//   while(curr) {
+//       set.add(curr);
+//       curr = curr.next;
+//       if(set.has(curr)) {
+//       return true;
+//   }
+//   }
+  
+//   return false;
+// };
+
+/* Palindrome in Linked List*/
+
+// var isPalindrome = function(head) {
+//   //find middle
+//   let slow = head;
+//   let fast = head;
+//   while(fast && fast.next) {
+//       slow = slow.next;
+//       fast = fast.next.next;
+//   }
+ 
+//  //reverse second list
+
+//  let prev = null;
+//  let curr = slow;
+//  while(curr) {
+//   let temp = curr.next;
+//   curr.next = prev;
+//   prev = curr;
+//   curr = temp;
+//  }
+
+//  let firstList = head;
+//  let secondList = prev;
+//  while(secondList) {
+//   if(firstList.val != secondList.val) {
+//       return false;
+//   }
+//   firstList = firstList.next;
+//   secondList = secondList.next;
+//  }
+//  return true;
+  
+// };
 }
 
