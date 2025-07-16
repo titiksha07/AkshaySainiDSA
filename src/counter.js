07f49d35c0cb2763200d10752483c79eb77fcf34
titiksha07 <titiksha0410@gmail.com>
2025-07-16 22:22:11 +0530

    ll

--- src/counter.js
+++ src/counter.js
@@ -539,6 +539,31 @@ console.log(ll);
 //   return newHead;
 
 // };
+
+/* Swap Nodes in pairs */
+
+// var swapPairs = function(head) {
+//      if(!head || !head.next) {
+//         return head;
+//     }
+//     let dummy = new ListNode();
+//     dummy.next = head;
+//     let p = dummy;
+//     let c = head;
+//     let n = head.next;
+   
+//     while(c && n) {
+//         p.next = n;
+//         c.next = n.next;
+//         n.next = c;
+//         p = c;
+//         c = p.next;
+//         n = c && c.next;
+
+//     }
+//     return dummy.next;
+
+// };
 }
 
 
