5be4630278538d316d24c8afd19f9def7554c5f0
titiksha07 <titiksha0410@gmail.com>
2025-07-16 16:54:03 +0530

    ll

--- src/counter.js
+++ src/counter.js
@@ -443,6 +443,26 @@ console.log(ll);
 //     }
 //     return head;
 // };
+
+
+/*Odd Even List */
+
+// var oddEvenList = function(head) {
+//   while (!head || !head.next) {
+//       return head;
+//   }
+// let odd = head;
+// let even = head.next;
+// let evenStart = even;
+// while(odd.next && even.next) {
+//   odd.next = odd.next.next;
+//   even.next = even.next.next;
+//   odd = odd.next;
+//   even = even.next;
+// }
+// odd.next = evenStart
+// return head;
+// };
 }
 
 
